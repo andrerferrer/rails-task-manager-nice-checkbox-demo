8be8a9b15c40e3dfae40124fa226e26d6b0bdb92
Andre Ferrer <andrerferrer@hotmail.com>
2020-08-25 15:43:29 -0300

    generate the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks, only: %i[ index show new create destroy ]
+
+  patch 'tasks/:id/complete', to: 'tasks#complete', as: :complete_task
 end
